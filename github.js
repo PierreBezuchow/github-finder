38ee24dd19f6ca6c71d238a77faf7d9ee8c2db5c
PierreBezuchow <axel@starosta.pl>
2018-05-23 19:13:21 +0200

    Added display user repo function

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = '8fcb9c82d01c54fc084e';
     this.client_secret = '1d760b6cf9f9a53b2a2e0d5706d3fe14c3dba218';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
 
   } 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile 
+      profile,
+      repos 
     } //if profile: profile then you can shorten it
 
   }
